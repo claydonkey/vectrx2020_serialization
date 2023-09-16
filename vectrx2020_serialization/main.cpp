dd5401face8cbc992294d37c3f00882c2c24110d
thinkstation <anthony@claydonkey.com>
2023-09-16 18:12:01 +0100

    mistook packed size for point size when copying buffer for transfer

--- vectrx2020_serialization/main.cpp
+++ vectrx2020_serialization/main.cpp
@@ -77,8 +77,6 @@ void Vecint64ToVecByte(uint8_t a[], uint64_t n) {
 	memcpy(a, &n, 8);
 	n = n >> 32;
 	memcpy(a + 3, &n, 4);
-	// n = n >> 16;
-	// memcpy(a + 4, &n, 3);
 }
 
 
@@ -100,26 +98,26 @@ int main()
 
 	//serialize data
 
-	vector<dvg_vec> out_points;
+	vector<dvg_vec> out_points_buff;
 
 	for (int i = 0; i < NUM_POINTS; i++)
 	{
 		dvg_vec point = { 0 };
-		point.val = dvec.val;// rand64();
+		point.val = rand64();
 
 		point.colors.color = (!(i % 2)) ? false : true;
 		if (!point.colors.color)
 			point.colors.rgb = 0;
 
-		out_points.push_back(point);
+		out_points_buff.push_back(point);
 		printf("with_color=%x x=%x y=%x r=%x g=%x b=%x \n\r", point.colors.color, point.pnt.x, point.pnt.y, point.pnt.r, point.pnt.g, point.pnt.b);
 	}
 
 	//serialize
-	printf("sending size: %lli\n\r", out_points.size());
+	printf("sending size: %lli\n\r", out_points_buff.size());
 
 	vector<uint8_t> out_meta_points;
-	vector<uint8_t> out_m_crunched_pnts;
+	vector<uint8_t> out_packed_pnts;
 
 	int cmd = FLAG_COMPLETE;
 	string header = "$cmd" + to_string(cmd) + " ";
@@ -128,7 +126,7 @@ int main()
 	uint32_t out_bit_iter = 0;
 	uint8_t meta_byte = 0;
 
-	for (dvg_vec& pnt : out_points)
+	for (dvg_vec& pnt : out_points_buff)
 	{
 
 		uint8_t m_ar[8];
@@ -136,9 +134,9 @@ int main()
 		int64ToByte(m_ar, pnt.val);
 
 		if (pnt.colors.color) {
-			out_m_crunched_pnts.push_back(m_ar[2]); //r
-			out_m_crunched_pnts.push_back(m_ar[1]);  // g
-			out_m_crunched_pnts.push_back(m_ar[0]); // b
+			out_packed_pnts.push_back(m_ar[2]); //r
+			out_packed_pnts.push_back(m_ar[1]);  // g
+			out_packed_pnts.push_back(m_ar[0]); // b
 
 		}
 		if (!(out_bit_iter % 8) && out_bit_iter) {
@@ -147,33 +145,34 @@ int main()
 		}
 
 		meta_byte |= pnt.colors.color << (out_bit_iter % 8);
-		out_m_crunched_pnts.push_back(m_ar[4]); //xy (backwards)
-		out_m_crunched_pnts.push_back(((m_ar[5] & 0x0F) << 4) | ((m_ar[6] & 0xF0) >> 4));
-		out_m_crunched_pnts.push_back(((m_ar[6] & 0x0F) << 4) | (m_ar[7] & 0x0F));
+		out_packed_pnts.push_back(m_ar[4]); //xy (backwards)
+		out_packed_pnts.push_back(((m_ar[5] & 0x0F) << 4) | ((m_ar[6] & 0xF0) >> 4));
+		out_packed_pnts.push_back(((m_ar[6] & 0x0F) << 4) | (m_ar[7] & 0x0F));
 		out_bit_iter++;
 	}
 	out_meta_points.push_back(meta_byte);
 
-	uint8_t out_points_size[4];
-	size_t out_p_size = out_points.size();
+	uint8_t meta_out_points_size[4];
 
-	int32ToByte(out_points_size, out_p_size);
+	uint32_t out_packed_size = out_packed_pnts.size();
+
+	int32ToByte(meta_out_points_size, out_points_buff.size());
 
 	for (int i = 3; i >= 0; i--)
-		out_meta_points.insert(out_meta_points.begin() + SIZEOF_HEADER, out_points_size[i]);
+		out_meta_points.insert(out_meta_points.begin() + SIZEOF_HEADER, meta_out_points_size[i]);
 
 	uint8_t* out_meta_buff = out_meta_points.data();
-	uint8_t* out_points_buff = out_m_crunched_pnts.data();
+	uint8_t* out_packed_points_buff = out_packed_pnts.data();
 	uint32_t out_meta_size = out_meta_points.size();
 
 	//transfer
 
 	uint8_t* in_meta_buff = NULL;
-	uint8_t* in_points_buff = NULL;
+	uint8_t* in_packed_points_buff = NULL;
 
-	if (out_p_size) {
-		in_points_buff = (uint8_t*)malloc(out_p_size);
-		memcpy(in_points_buff, out_points_buff, out_p_size);
+	if (out_packed_size) {
+		in_packed_points_buff = (uint8_t*)malloc(out_packed_size);
+		memcpy(in_packed_points_buff, out_packed_points_buff, out_packed_size);
 	}
 	if (out_meta_size) {
 		in_meta_buff = (uint8_t*)malloc(out_meta_size);
@@ -183,17 +182,17 @@ int main()
 	//deserialize
 
 	uint32_t int_bit_iter = 0;
-	uint32_t in_p_size;
+
 	uint32_t with_color_size = 0;
-	//vector<dvg_vec> in_points;
+	vector<dvg_vec> in_points;
 	bool with_color[NUM_POINTS] = { 0 };
 	int byte_iter;
-
-	ByteToint32(out_meta_buff + +SIZEOF_HEADER, &in_p_size);
+	uint32_t in_p_size;
+	ByteToint32(in_meta_buff + +SIZEOF_HEADER, &in_p_size);
 
 	for (byte_iter = 0; byte_iter < in_p_size - 8; byte_iter += 8)
 	{
-		uint8_t meta_byte = *(out_meta_buff + (SIZEOF_HEADER + SIZEOF_LEN) + byte_iter / 8);
+		uint8_t meta_byte = *(in_meta_buff + (SIZEOF_HEADER + SIZEOF_LEN) + byte_iter / 8);
 		for (int_bit_iter = 0; int_bit_iter < 8; int_bit_iter++) {
 			uint8_t bit = 1 & meta_byte >> int_bit_iter;
 			with_color[int_bit_iter + byte_iter] = (bit);
@@ -202,7 +201,7 @@ int main()
 	}
 
 	if (in_p_size % 8)
-		uint8_t meta_byte = *(out_meta_buff + (SIZEOF_HEADER + SIZEOF_LEN + 1) + byte_iter / 8);
+		uint8_t meta_byte = *(in_meta_buff + (SIZEOF_HEADER + SIZEOF_LEN + 1) + byte_iter / 8);
 
 	for (int_bit_iter = 0; int_bit_iter < in_p_size % 8; int_bit_iter++) {
 		uint8_t bit = 1 & meta_byte >> int_bit_iter;
@@ -214,8 +213,8 @@ int main()
 
 
 
-	uint8_t* start_points_buff = in_points_buff;
-	if (in_points_buff != NULL) {
+	uint8_t* start_points_buff = in_packed_points_buff;
+	if (in_packed_points_buff != NULL) {
 		for (int pts = 0; pts < with_color_size; pts++)
 		{
 			dvg_vec point;
@@ -226,40 +225,40 @@ int main()
 
 			if (point.colors.color)
 			{
-				point.pnt.r = *(in_points_buff);
-				point.pnt.g = *(in_points_buff + 1);
-				point.pnt.b = *(in_points_buff + 2);
+				point.pnt.r = *(in_packed_points_buff);
+				point.pnt.g = *(in_packed_points_buff + 1);
+				point.pnt.b = *(in_packed_points_buff + 2);
 
-				in_points_buff += 3;
+				in_packed_points_buff += 3;
 			}
 
-			point.pnt.y = ((*(in_points_buff + 1) & 0xF0) << 4) + *(in_points_buff);
-			point.pnt.x = ((*(in_points_buff + 2) & 0x0F) << 8) + ((*(in_points_buff + 2) & 0xF0) >> 4) + ((*(in_points_buff + 1) & 0x0F) << 4);
+			point.pnt.y = ((*(in_packed_points_buff + 1) & 0xF0) << 4) + *(in_packed_points_buff);
+			point.pnt.x = ((*(in_packed_points_buff + 2) & 0x0F) << 8) + ((*(in_packed_points_buff + 2) & 0xF0) >> 4) + ((*(in_packed_points_buff + 1) & 0x0F) << 4);
 
-			in_points_buff += 3;
+			in_packed_points_buff += 3;
 
 			//DO SOMETHING
 			printf("with_color=%x x=%x y=%x r=%x g=%x b=%x \n\r", point.colors.color, (point.pnt.x), point.pnt.y, point.pnt.r, point.pnt.g, point.pnt.b);
 			//OR STORE
-		//	in_points.push_back(point);
+			//in_points.push_back(point);
 		}
 
 		if (in_p_size) {
 			free(start_points_buff);
-			in_points_buff = NULL;
+			in_packed_points_buff = NULL;
 			start_points_buff = NULL;
 		}
 		if (in_meta_buff) {
 			free(in_meta_buff);
 			in_meta_buff = NULL;
 		}
-		/*
-				for (dvg_vec& pnt : in_points)
-				{
-					printf("with_color=%x x=%x y=%x r=%x g=%x b=%x \n\r", pnt.colors.color, (pnt.pnt.x), pnt.pnt.y, pnt.pnt.r, pnt.pnt.g, pnt.pnt.b);
-				}
-				printf("results size: %lli\n\r", in_points.size());
-		*/
+
+	//	for (dvg_vec& pnt : in_points)
+	//	{
+	//		printf("with_color=%x x=%x y=%x r=%x g=%x b=%x \n\r", pnt.colors.color, (pnt.pnt.x), pnt.pnt.y, pnt.pnt.r, pnt.pnt.g, pnt.pnt.b);
+	//	}
+	//	printf("results size: %lli\n\r", in_points.size());
+
 	}
 
 }
