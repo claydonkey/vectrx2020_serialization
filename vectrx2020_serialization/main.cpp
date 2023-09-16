27661b4b1639650821c904e19dfabd61e0de9a5c
thinkstation <anthony@claydonkey.com>
2023-09-16 16:37:25 +0100

    hmmph

--- vectrx2020_serialization/main.cpp
+++ vectrx2020_serialization/main.cpp
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
-/*
- * This file is part of the vectrx2020 distribution (https://github.com/claydonkey/vectrx2020).
- * Copyright (c) 2023 Anthony Campbell
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-=======
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
->>>>>>> origin/master
 
 #include <iostream>
 #include <vector>
@@ -26,14 +7,11 @@
 #include <iostream>
 #include <string> 
 
-<<<<<<< HEAD
 #include <stdio.h>
 #include <string.h>
-=======
 
 
 
->>>>>>> origin/master
 
 #ifdef __GNUC__
 #define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
@@ -95,9 +73,6 @@ void int32ToByte(uint8_t a[], uint32_t n) {
 	memcpy(a, &n, 4);
 
 }
-<<<<<<< HEAD
-
-=======
 void Vecint64ToVecByte(uint8_t a[], uint64_t n) {
 	memcpy(a, &n, 8);
 	n = n >> 32;
@@ -105,7 +80,7 @@ void Vecint64ToVecByte(uint8_t a[], uint64_t n) {
 	// n = n >> 16;
 	// memcpy(a + 4, &n, 3);
 }
->>>>>>> origin/master
+
 
 uint64_t rand64(void)
 {
@@ -155,10 +130,7 @@ int main()
 
 	for (dvg_vec& pnt : out_points)
 	{
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 		uint8_t m_ar[8];
 
 		int64ToByte(m_ar, pnt.val);
@@ -183,11 +155,7 @@ int main()
 	out_meta_points.push_back(meta_byte);
 
 	uint8_t out_points_size[4];
-<<<<<<< HEAD
-	uint32_t out_p_size = out_points.size();
-=======
 	size_t out_p_size = out_points.size();
->>>>>>> origin/master
 
 	int32ToByte(out_points_size, out_p_size);
 
@@ -196,7 +164,6 @@ int main()
 
 	uint8_t* out_meta_buff = out_meta_points.data();
 	uint8_t* out_points_buff = out_m_crunched_pnts.data();
-<<<<<<< HEAD
 	uint32_t out_meta_size = out_meta_points.size();
 
 	//transfer
@@ -212,95 +179,13 @@ int main()
 		in_meta_buff = (uint8_t*)malloc(out_meta_size);
 		memcpy(in_meta_buff, out_meta_buff, out_meta_size);
 	}
-=======
-
->>>>>>> origin/master
 
 	//deserialize
 
 	uint32_t int_bit_iter = 0;
 	uint32_t in_p_size;
 	uint32_t with_color_size = 0;
-<<<<<<< HEAD
-//	vector<dvg_vec> in_points;
-	bool with_color[NUM_POINTS] = { 0 };
-	int byte_iter;
-
-	if (in_meta_buff != NULL) {
-		if (strstr((char* const)in_meta_buff, "$cmd4 ")) {
-
-			ByteToint32(in_meta_buff + +SIZEOF_HEADER, &out_p_size);
-
-			for (byte_iter = 0; byte_iter < out_p_size - 8; byte_iter += 8)
-			{
-				uint8_t meta_byte = *(out_meta_buff + (SIZEOF_HEADER + SIZEOF_LEN) + byte_iter / 8);
-				for (int_bit_iter = 0; int_bit_iter < 8; int_bit_iter++) {
-					uint8_t bit = 1 & meta_byte >> int_bit_iter;
-					with_color[int_bit_iter + byte_iter] = (bit);
-					with_color_size++;
-				}
-			}
-
-			if (out_p_size % 8)
-				uint8_t meta_byte = *(out_meta_buff + (SIZEOF_HEADER + SIZEOF_LEN + 1) + byte_iter / 8);
-
-			for (int_bit_iter = 0; int_bit_iter < out_p_size % 8; int_bit_iter++) {
-				uint8_t bit = 1 & meta_byte >> int_bit_iter;
-				with_color[int_bit_iter + byte_iter] = (bit);
-				with_color_size++;
-			}
-			if (with_color_size != out_p_size)
-				printf("error in tranmission\n\r");
-
-			uint8_t* start_points_buff = in_points_buff;
-			if (in_points_buff != NULL) {
-				for (int pts = 0; pts < with_color_size; pts++)
-				{
-					dvg_vec point;
-					point.colors.color = with_color[pts];
-					point.pnt.r = 0;
-					point.pnt.g = 0;
-					point.pnt.b = 0;
-
-					if (point.colors.color)
-					{
-						point.pnt.r = *(in_points_buff);
-						point.pnt.g = *(in_points_buff + 1);
-						point.pnt.b = *(in_points_buff + 2);
-
-						in_points_buff += 3;
-					}
-
-					point.pnt.y = ((*(in_points_buff + 1) & 0xF0) << 4) + *(in_points_buff);
-					point.pnt.x = ((*(in_points_buff + 2) & 0x0F) << 8) + ((*(in_points_buff + 2) & 0xF0) >> 4) + ((*(in_points_buff + 1) & 0x0F) << 4);
-
-					in_points_buff += 3;
-				//	in_points.push_back(point);
-
-					printf("with_color=%x x=%x y=%x r=%x g=%x b=%x \n\r", point.colors.color, (point.pnt.x), point.pnt.y, point.pnt.r, point.pnt.g, point.pnt.b);
-				}
-
-			//	for (dvg_vec& pnt : in_points)
-			//	{
-			//		printf("with_color=%x x=%x y=%x r=%x g=%x b=%x \n\r", pnt.colors.color, (pnt.pnt.x), pnt.pnt.y, pnt.pnt.r, pnt.pnt.g, pnt.pnt.b);
-			//	}
-			//	printf("results size: %lli\n\r", in_points.size());
-			}
-			if (in_points_buff) {
-				free(start_points_buff);
-				start_points_buff = NULL;
-			}
-		}
-		if (in_meta_buff)
-		free(in_meta_buff);
-	
-	
-	}
-	
-
-		
-=======
-	vector<dvg_vec> in_points;
+	//vector<dvg_vec> in_points;
 	bool with_color[NUM_POINTS] = { 0 };
 	int byte_iter;
 
@@ -329,35 +214,52 @@ int main()
 
 
 
-	for (int pts = 0; pts < with_color_size; pts++)
-	{
-		dvg_vec point;
-		point.colors.color = with_color[pts];
-		point.pnt.r = 0;
-		point.pnt.g = 0;
-		point.pnt.b = 0;
-
-		if (point.colors.color)
+	uint8_t* start_points_buff = in_points_buff;
+	if (in_points_buff != NULL) {
+		for (int pts = 0; pts < with_color_size; pts++)
 		{
-			point.pnt.r = *(out_points_buff);
-			point.pnt.g = *(out_points_buff + 1);
-			point.pnt.b = *(out_points_buff + 2);
+			dvg_vec point;
+			point.colors.color = with_color[pts];
+			point.pnt.r = 0;
+			point.pnt.g = 0;
+			point.pnt.b = 0;
 
-			out_points_buff += 3;
+			if (point.colors.color)
+			{
+				point.pnt.r = *(in_points_buff);
+				point.pnt.g = *(in_points_buff + 1);
+				point.pnt.b = *(in_points_buff + 2);
+
+				in_points_buff += 3;
+			}
+
+			point.pnt.y = ((*(in_points_buff + 1) & 0xF0) << 4) + *(in_points_buff);
+			point.pnt.x = ((*(in_points_buff + 2) & 0x0F) << 8) + ((*(in_points_buff + 2) & 0xF0) >> 4) + ((*(in_points_buff + 1) & 0x0F) << 4);
+
+			in_points_buff += 3;
+
+			//DO SOMETHING
+			printf("with_color=%x x=%x y=%x r=%x g=%x b=%x \n\r", point.colors.color, (point.pnt.x), point.pnt.y, point.pnt.r, point.pnt.g, point.pnt.b);
+			//OR STORE
+		//	in_points.push_back(point);
 		}
 
-		point.pnt.y = ((*(out_points_buff + 1) & 0xF0) << 4) + *(out_points_buff);
-		point.pnt.x = ((*(out_points_buff + 2) & 0x0F) << 8) + ((*(out_points_buff + 2) & 0xF0) >> 4) + ((*(out_points_buff + 1) & 0x0F) << 4);
-
-		out_points_buff += 3;
-		in_points.push_back(point);
+		if (in_p_size) {
+			free(start_points_buff);
+			in_points_buff = NULL;
+			start_points_buff = NULL;
+		}
+		if (in_meta_buff) {
+			free(in_meta_buff);
+			in_meta_buff = NULL;
+		}
+		/*
+				for (dvg_vec& pnt : in_points)
+				{
+					printf("with_color=%x x=%x y=%x r=%x g=%x b=%x \n\r", pnt.colors.color, (pnt.pnt.x), pnt.pnt.y, pnt.pnt.r, pnt.pnt.g, pnt.pnt.b);
+				}
+				printf("results size: %lli\n\r", in_points.size());
+		*/
 	}
 
-	for (dvg_vec& pnt : in_points)
-	{
-		printf("with_color=%x x=%x y=%x r=%x g=%x b=%x \n\r", pnt.colors.color, (pnt.pnt.x), pnt.pnt.y, pnt.pnt.r, pnt.pnt.g, pnt.pnt.b);
-	}
-	printf("results size: %lli\n\r", in_points.size());
-
->>>>>>> origin/master
 }
